45e028b160aeefed340a6f16cf831b0b09c3b3d3
Deekshitha Kanduri <kandurideekshitha99@gmail.com>
2024-05-29 18:42:52 +0530

    Updated home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MessageComponent } from './message/message.component';
+
 
 @Component({
+  standalone:true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[MessageComponent]
 })
 export class AppComponent {
   title = 'training';
